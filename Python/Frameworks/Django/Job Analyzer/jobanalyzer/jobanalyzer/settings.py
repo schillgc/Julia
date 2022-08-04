c99f16b8777d084d4b98593a66e05bee8968576a
Gavin Schilling <schillgc@hotmail.com>
2022-08-03 21:03:07 -0400

    Remove Redundancy

--- Python/Frameworks/Django/Job Analyzer/jobanalyzer/jobanalyzer/settings.py
+++ Python/Frameworks/Django/Job Analyzer/jobanalyzer/jobanalyzer/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'address',
     'djmoney',
     'phone_field',
-    'theanalyzer',
 ]
 
 LANGUAGES = [
@@ -145,5 +144,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS = (os.path.join(BASE_DIR, 'media'), )
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
