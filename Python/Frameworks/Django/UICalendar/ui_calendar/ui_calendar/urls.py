f1099f8e13387834a248ec64dbacf843a6ba91d2
Gavin Schilling <schillgc@hotmail.com>
2022-08-17 00:33:33 -0400

    Update import to re_path

--- Python/Frameworks/Django/UICalendar/ui_calendar/ui_calendar/urls.py
+++ Python/Frameworks/Django/UICalendar/ui_calendar/ui_calendar/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^calendar/', include('calendarium.urls')),
+    re_path(r'^calendar/', include('calendarium.urls')),
 ]
