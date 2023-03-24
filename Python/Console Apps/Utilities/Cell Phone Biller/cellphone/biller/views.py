4088cd4f5a4a3e576f851930bb6fe01e328aa63b
Gavin Schilling <schillgc@hotmail.com>
2023-03-24 13:45:28 -0400

    Initial Commit

--- Python/Console Apps/Utilities/Cell Phone Biller/cellphone/biller/views.py
+++ Python/Console Apps/Utilities/Cell Phone Biller/cellphone/biller/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Phone
+class PhoneDetailView(generic.DetailView):
+    model = Phone
+    template_name = 'biller/detail.html'
