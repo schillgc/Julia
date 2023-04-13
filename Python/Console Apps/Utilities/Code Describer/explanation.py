af0da308e448ed0353eb4489636f9f6744e8ffa3
Gavin Schilling <schillgc@hotmail.com>
2023-04-13 02:07:19 -0400

    Initial Commit

---
+++ Python/Console Apps/Utilities/Code Describer/explanation.py
@@ -0,0 +1,15 @@
+import os
+import config
+import openai
+
+openai.api_key = os.getenv(config.api_key)
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=input("Input code to be explained what it does:\n"),
+  temperature=0,
+  max_tokens=150,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
