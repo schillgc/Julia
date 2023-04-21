56a4b717b243d7cd808e762b3ea5a498df0075dc
Gavin Schilling <schillgc@hotmail.com>
2023-04-21 07:36:24 -0400

    Add Instructor-related URLs

--- Python/Frameworks/Django/Hayden/Education/urls.py
+++ Python/Frameworks/Django/Hayden/Education/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import CreditDetailView, CreditIndexView, InstitutionDetailView, InstitutionIndexView
+from .views import CreditDetailView, CreditIndexView, InstitutionDetailView, InstitutionIndexView, InstructorDetailView, InstructorIndexView
 
 app_name = 'Education'
 urlpatterns = [
     path('class/<int:pk>/', CreditDetailView.as_view(), name='credit-detail'),
     path('school/<int:pk>/', InstitutionDetailView.as_view(), name='institution-detail'),
+    path('teacher/<int:pk>/', InstructorDetailView.as_view(), name='teacher-detail'),
     path('classes', CreditIndexView.as_view(), name='classes'),
+    path('teachers', InstructorIndexView.as_view(), name='instructors'),
     path('schools', InstitutionIndexView.as_view(), name='schools'),
 ]
