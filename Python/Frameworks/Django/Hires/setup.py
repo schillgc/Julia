45db3f765e263b4b5dc926540fcae8a47ac9a8a9
Gavin Schilling <schillgc@hotmail.com>
2023-07-11 11:11:15 -0400

    Initial Commit

---
+++ Python/Frameworks/Django/Hires/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Hires',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GNU Affero General Public License v3',
+    author='Gavin Schilling',
+    author_email='schillgc@gmail.com',
+    description='Perspective Job Database'
+)
