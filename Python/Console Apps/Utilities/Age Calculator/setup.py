ccaae4eb6be85a67c961aa7ebe7898e2a0715423
Gavin Schilling <schillgc@hotmail.com>
2023-07-11 11:05:04 -0400

    Initial Commit

---
+++ Python/Console Apps/Utilities/Age Calculator/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Age Calculator',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GNU Affero General Public License v3',
+    author='Gavin Schilling',
+    author_email='schillgc@gmail.com',
+    description=''
+)
