f5880bfbdb174081ada52d5e4c5456c760cccbd1
Gavin Schilling <schillgc@hotmail.com>
2023-09-10 20:19:21 -0400

    Add prepopulated slugs

--- Python/Frameworks/Django/Hayden/Education/admin.py
+++ Python/Frameworks/Django/Hayden/Education/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 
 from .models import Credit, Institution, Instructor
 
-admin.site.register(Credit)
-admin.site.register(Institution)
-admin.site.register(Instructor)
+# admin.site.register(Credit)
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('grade_level', 'name',)}
+
+# admin.site.register(Institution)
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+# admin.site.register(Instructor)
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('first_name', 'last_name',)}
+
+class ClassInstuctorsInline(admin.TabularInline):
+    model = Credit
+    readonly_fields = ('teacher')
+    extra = 1
