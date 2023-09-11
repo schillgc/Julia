d4a42bfc16ccaa6f192d47cacd2d9be6107f5cf0
Gavin Schilling <schillgc@hotmail.com>
2023-09-10 20:23:21 -0400

    Update detail urls

--- Python/Frameworks/Django/Hayden/Education/urls.py
+++ Python/Frameworks/Django/Hayden/Education/urls.py
@@ -5,9 +5,9 @@ from .views import CreditDetailView, CreditIndexView, InstitutionDetailView, Ins
 
 app_name = 'Education'
 urlpatterns = [
-    path('class/<int:pk>/', CreditDetailView.as_view(), name='credit-detail'),
-    path('school/<int:pk>/', InstitutionDetailView.as_view(), name='institution-detail'),
-    path('teacher/<int:pk>/', InstructorDetailView.as_view(), name='teacher-detail'),
+    path('class/<slug>/', CreditDetailView.as_view(), name='credit-detail'),
+    path('school/<slug>/', InstitutionDetailView.as_view(), name='institution-detail'),
+    path('teacher/<slug>/', InstructorDetailView.as_view(), name='teacher-detail'),
     path('classes', CreditIndexView.as_view(), name='classes'),
     path('teachers', InstructorIndexView.as_view(), name='instructors'),
     path('schools', InstitutionIndexView.as_view(), name='schools'),
