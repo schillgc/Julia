35545f790cfd9aaa9498b73162654312126c69ab
Gavin Schilling <schillgc@hotmail.com>
2023-09-28 00:07:14 -0500

    Modernize Code

--- Python/Console Apps/Utilities/MailChimp API/mailchimp-api.py
+++ Python/Console Apps/Utilities/MailChimp API/mailchimp-api.py
@@ -1,34 +1,23 @@
-from mailchimp3 import MailChimp
+import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing.api_client import ApiClientError
+import mailchimp_transactional as MailchimpTransactional
+from mailchimp_transactional.api_client import ApiClientError
 
 
-def pretty(d, indent=0):
-    for key, value in d.items():
-        print('\t' * indent + str(key) + '\n')
-        if isinstance(value, dict):
-            pretty(value, indent + 1)
-        else:
-            print('\t' * (indent + 1) + str(value) + '\n')
+# try:
+#   client = MailchimpMarketing.Client()
+#   client.set_config({
+#     "api_key": "9d006200f84026629fffec6e0eaa5099",
+#     "server": "us14"
+#   })
+#   response = client.ping.get()
+#   print(response)
+# except ApiClientError as error:
+#   print(error)
 
-
-def menu():
-    print("""
-    MailChimp API Menu:
-    1.  List campaigns
-    2.  List Subscribers
-    3.  Exit
-    """)
-    choice = input("Enter your numerical selection, please: ")
-
-    if choice == "1":
-        pretty(client.campaigns.all(get_all=True))
-    elif choice == "2":
-        print("The following are the available lists with their list ID:\n",
-              client.lists.all(get_all=True, fields="lists.name,lists.id"))
-        choice = input("What is the list ID of the member list you wish to see? ")
-        pretty(client.lists.members.all(choice, get_all=True))
-    else:
-        exit()
-
-
-client = MailChimp(mc_api='2b05f96cead1dc46d3fcd595cb40f76f-us13', mc_user='optimalleader')
-menu()
+try:
+  mailchimp = MailchimpTransactional.Client('9d006200f84026629fffec6e0eaa5099-us14')
+  response = mailchimp.users.ping()
+  print('API called successfully: {}'.format(response))
+except ApiClientError as error:
+  print('An exception occurred: {}'.format(error.text))
