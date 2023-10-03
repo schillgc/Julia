b0aee4880924a2351d5550576a9988956d761966
Gavin Schilling <schillgc@hotmail.com>
2023-10-02 22:27:14 -0500

    Add news url

--- Python/Frameworks/Django/semmLaw/semmlaw/website/urls.py
+++ Python/Frameworks/Django/semmLaw/semmlaw/website/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import index, Bio
+from .views import index, Bio, news
 
 urlpatterns = [
     path('', index, name='index'),
     path('firm/<slug:slug>/', Bio.as_view(), name='attorney_bio'),
-    # path('news/', News, name='news'),
+    path('firm/<slug:slug>/news', news, name='news'),
 ]
