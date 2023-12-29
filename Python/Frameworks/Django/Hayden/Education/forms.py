d4182dccf5bde3ddaa98836cd57808319cf00da3
Gavin Schilling <schillgc@hotmail.com>
2023-12-28 21:55:39 -0500

    Update forms

--- Python/Frameworks/Django/Hayden/Education/forms.py
+++ Python/Frameworks/Django/Hayden/Education/forms.py
@@ -1,5 +1,11 @@
 from address.forms import AddressField
 from django import forms
+from .models import Credit
+
+class CreditForm(forms.ModelForm):
+    class Meta:
+        model = Credit
+        fields = ['school', 'name', 'grade_level', 'subject', 'course_number', 'section', 'track', 'clep_exam', 'registered', 'grade_percentage', 'term', 'instructor']
 
 
 class PersonForm(forms.Form):
