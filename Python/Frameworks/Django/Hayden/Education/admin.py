4412d2b0ab7e683bc97e1be156938c1053a41820
Gavin Schilling <schillgc@hotmail.com>
2023-12-29 03:21:10 -0500

    Add Documentation

--- Python/Frameworks/Django/Hayden/Education/admin.py
+++ Python/Frameworks/Django/Hayden/Education/admin.py
@@ -1,21 +1,35 @@
+"""
+Django Admin Configuration
+"""
+
 from django.contrib import admin
 
 from .models import Credit, Institution, Instructor
 
-# admin.site.register(Credit)
+
 @admin.register(Credit)
 class CreditAdmin(admin.ModelAdmin):
+    """
+    Django Admin Configuration for Credits
+    """
     prepopulated_fields = {'slug': ('grade_level', 'name',)}
 
-# admin.site.register(Institution)
+
 @admin.register(Institution)
 class InstitutionAdmin(admin.ModelAdmin):
+    """
+    Django Admin Configuration for Institutions
+    """
     prepopulated_fields = {'slug': ('name',)}
 
-# admin.site.register(Instructor)
+
 @admin.register(Instructor)
 class InstructorAdmin(admin.ModelAdmin):
+    """
+    Django Admin Configuration for Instructors
+    """
     prepopulated_fields = {'slug': ('first_name', 'last_name',)}
+
 
 class ClassInstuctorsInline(admin.TabularInline):
     model = Credit
