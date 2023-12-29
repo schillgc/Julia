70835284c57aab9deb374818556c83f3c082934b
Gavin Schilling <schillgc@hotmail.com>
2023-12-29 03:25:16 -0500

    Add Documentation

--- Python/Frameworks/Django/Hayden/Education/apps.py
+++ Python/Frameworks/Django/Hayden/Education/apps.py
@@ -1,5 +1,16 @@
+"""
+File: apps.py
+Author: Gavin Schilling
+
+AppConfig for Education
+"""
+
 from django.apps import AppConfig
 
 
 class EducationConfig(AppConfig):
+    """
+    AppConfig for the Education application
+    """
+
     name = 'Education'
