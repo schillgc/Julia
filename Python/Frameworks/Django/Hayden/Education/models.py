bf1cfdf7e232a07ef0a39156b9a2593477e194c6
Gavin Schilling <schillgc@hotmail.com>
2023-12-29 02:47:05 -0500

    Add Documentation

--- Python/Frameworks/Django/Hayden/Education/models.py
+++ Python/Frameworks/Django/Hayden/Education/models.py
@@ -2,12 +2,37 @@ from address.models import AddressField
 from django.core.validators import EmailValidator
 from django.db import models
 from django.db.models import TextChoices
-from django.urls import reverse
 from djmoney.models.fields import MoneyField
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Institution(models.Model):
+    """
+    A model representing an educational institution.
+
+    Attributes:
+        name (str): The name of the institution.
+        next_year_full_tuition (djmoney.models.fields.MoneyField): The full tuition for the next school year.
+        registration_fee (djmoney.models.fields.MoneyField): The registration fee for new students.
+        student_activity_fee (djmoney.models.fields.MoneyField): The fee for student activities.
+        headmaster (str): The name of the headmaster.
+        address (address.models.AddressField): The address of the institution.
+        phone_number (phonenumber_field.modelfields.PhoneNumberField): The main phone number of the institution.
+        fax_number (phonenumber_field.modelfields.PhoneNumberField): The main fax number of the institution.
+        admissions_director (str): The name of the admissions director.
+        website (str): The website of the institution.
+        application_received (bool): A boolean indicating whether the application has been received.
+        application_submitted (bool): A boolean indicating whether the application has been submitted.
+        toured (bool): A boolean indicating whether the campus has been toured.
+        teacher_recommendations_requested (bool): A boolean indicating whether teacher recommendations have been requested.
+        teacher_recommendations_submitted (bool): A boolean indicating whether teacher recommendations have been submitted.
+        accepted (bool): A boolean indicating whether the institution has been accepted.
+        financial_aid_requested (bool): A boolean indicating whether financial aid has been requested.
+        financial_aid_awarded (djmoney.models.fields.MoneyField): The amount of financial aid awarded.
+        description (str): A description of the institution.
+        slug (str): A unique slug for the institution.
+    """
+
     name = models.CharField(verbose_name="Name of the School", max_length=250)
     next_year_full_tuition = MoneyField(max_digits=7, decimal_places=0, default_currency='USD')
     registration_fee = MoneyField(max_digits=3, decimal_places=0, default_currency='USD')
@@ -37,11 +62,24 @@ class Institution(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('institution-detail', kwargs={'pk': self.pk})
-
 
 class Instructor(models.Model):
+    """
+    A model representing a teaching instructor.
+
+    Attributes:
+        first_name (str): The instructor's first name.
+        last_name (str): The instructor's last name.
+        email (str): The instructor's email address.
+        phone (str): The instructor's phone number.
+        title (str): The instructor's professional role title.
+        slug (str): A unique slug for the instructor.
+
+    Methods:
+        __str__(self): Returns the instructor's full name.
+
+    """
+
     first_name = models.CharField(verbose_name="Instructor's First Name", max_length=50, blank=True)
     last_name = models.CharField(verbose_name="Instructor's Last Name", max_length=50, blank=True)
     email = models.EmailField(verbose_name="Instructor's Email Address", blank=True, validators=[EmailValidator])
@@ -68,6 +106,33 @@ class Instructor(models.Model):
 
 
 class Credit(models.Model):
+    """
+    A model representing a graduation credit.
+
+    Attributes:
+        school (Institution): The institution that offers the credit.
+        name (str): The name of the credit.
+        grade_level (str): The grade level of the credit.
+        subject (str): The subject of the credit.
+        course_number (int): The course number of the credit.
+        section (int): The section of the credit.
+        track (str): The track of the credit.
+        clep_exam (bool): A boolean indicating whether the credit is a CLEP exam.
+        registered (bool): A boolean indicating whether the credit is registered.
+        grade_percentage (int): The raw course grade percentage.
+        term (str): The term of the credit.
+        instructor (list): A list of instructors for the credit.
+        slug (str): A unique slug for the credit.
+
+    Methods:
+        __str__(self): Returns the name of the credit.
+        unweighted_gpa(grade_percentage): Returns the unweighted GPA.
+        weighted_gpa(self): Returns the weighted GPA.
+        class_weight(self): Returns the class weight.
+        grade_equivalence(self): Returns the grade equivalence.
+
+    """
+
     class Subject(TextChoices):
         BUSINESS_AND_TECHNOLOGY = 'B', 'Business & Technology'
         ENGLISH = 'E', 'English'
@@ -113,7 +178,7 @@ class Credit(models.Model):
     grade_percentage = models.PositiveSmallIntegerField(verbose_name="Raw Course Grade Percentage", null=True,
                                                         blank=True)
     term = models.CharField(verbose_name="Class Weight", max_length=12, choices=Term.choices, blank=True)
-    instructor = models.ManyToManyField(Instructor, verbose_name="Course Teacher", blank=True)
+    instructor = models.ManyToManyField(Instructor, verbose_name="Course Teacher(s)", blank=True)
     slug = models.SlugField(unique=True)
 
     class Meta:
@@ -125,11 +190,20 @@ class Credit(models.Model):
         return (f"{self.term} of {self.track} {self.grade_level} {self.name} - Course GPA:"
                 f" {self.weighted_gpa} - Academic Credit(s): {self.class_weight}")
 
-    def get_absolute_url(self):
-        return reverse('credit-detail', kwargs={'pk': self.pk})
 
     @property
     def unweighted_gpa(grade_percentage):
+        """
+        Returns the unweighted GPA.
+
+        Args:
+            grade_percentage (int): The raw course grade percentage.
+
+        Returns:
+            float: The unweighted GPA.
+
+        """
+
         unweighted_gpa = 0.0
         unweighted_gpa += min((grade_percentage - 50) / 10, 4.0)
 
@@ -137,6 +211,14 @@ class Credit(models.Model):
 
     @property
     def weighted_gpa(self):
+        """
+        Returns the weighted GPA.
+
+        Returns:
+            float: The weighted GPA.
+
+        """
+
         weighted_gpa = 0.0
         unweighted_gpa = 0.0
 
@@ -158,6 +240,14 @@ class Credit(models.Model):
 
     @property
     def class_weight(self):
+        """
+        Returns the class weight.
+
+        Returns:
+            float: The class weight.
+
+        """
+
         if self.term in ["Semester", "Summer"]:
             return 0.5
         elif self.term == "Full Year":
@@ -167,6 +257,14 @@ class Credit(models.Model):
 
     @property
     def grade_equivalence(self):
+        """
+        Returns the grade equivalence.
+
+        Returns:
+            str: The grade equivalence.
+
+        """
+
         weighted_gpa = self.weighted_gpa
         if weighted_gpa >= 4 + (1 / 3):
             return 'A+'
