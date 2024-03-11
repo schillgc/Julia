3189c51651ffed287cac1d0d947be4bfc1f4ea03
Gavin Clay Schilling <schillgc@hotmail.com>
2024-03-10 21:44:05 -0400

    Code cleanup

--- Python/Frameworks/Django/Hayden/Education/templatetags/functions.py
+++ Python/Frameworks/Django/Hayden/Education/templatetags/functions.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 
 @register.filter
-@stringfilter
 def unslugify(value):
     """
     Convert a slug into a title.
@@ -15,25 +14,13 @@ def unslugify(value):
 
 
 @register.filter
-@stringfilter
 def roman_numerals(value):
     """
     Convert Arabic numerals to Roman numerals.
     For example, '1' becomes 'I', '2' becomes 'II', and so on.
     """
-    numeral_map = {
-        '1': 'I',
-        '2': 'II',
-        '3': 'III',
-        '4': 'IV',
-        '5': 'V'
-    }
-
-    result = ""
-
-    for char in value:
-        result += numeral_map.get(char, char)
-
+    numeral_map = {'1': 'I', '2': 'II', '3': 'III', '4': 'IV', '5': 'V'}
+    result = "".join(numeral_map.get(char, char) for char in value)
     return result
 
 
@@ -41,39 +28,49 @@ def convert_to_gpa_and_letter_grade(Credit):
     """
     Convert the credit's letter grade and raw score grade to GPA and weighted GPA.
     """
+    score_to_grade_mapping = {
+        90.00: 4.00,
+        89.00: 3.90,
+        88.00: 3.80,
+        87.00: 3.70,
+        86.00: 3.50,
+        85.00: 3.40,
+        84.00: 3.30,
+        83.00: 3.00,
+        82.00: 2.90,
+        81.00: 2.80,
+        80.00: 2.70,
+        79.00: 2.50,
+        78.00: 2.40,
+        77.00: 2.30,
+        76.00: 2.10,
+        75.00: 1.90,
+        74.00: 1.80,
+        73.00: 1.70,
+        72.00: 1.50,
+        71.00: 1.40,
+        70.00: 1.30,
+        69.00: 0.00
+    }
+
     gpa_mapping = {
-        'A+': 4.33,
-        'A': 4.00,
-        'A-': 3.67,
-        'B+': 3.33,
-        'B': 3.00,
-        'B-': 2.67,
-        'C+': 2.33,
-        'C': 2.00,
-        'C-': 1.67,
-        'D+': 1.33,
-        'D': 1.00,
-        'D-': 0.67,
-        'F': 0.00
+        4.33: 'A+',
+        4.00: 'A',
+        3.67: 'A-',
+        3.33: 'B+',
+        3.00: 'B',
+        2.67: 'B-',
+        2.33: 'C+',
+        2.00: 'C',
+        1.67: 'C-',
+        1.33: 'D+',
+        1.00: 'D',
+        0.67: 'D-',
+        0.00: 'F'
     }
 
     gpa = gpa_mapping.get(Credit.letter_grade, 0.00)
     weighted_gpa = gpa + 1 if Credit.required_exam == "AP" else gpa
-
-    score_to_grade_mapping = {
-        97.00: "A+",
-        93.00: "A",
-        90.00: "A-",
-        87.00: "B+",
-        83.00: "B",
-        80.00: "B-",
-        77.00: "C+",
-        73.00: "C",
-        70.00: "C-",
-        67.00: "D+",
-        63.00: "D",
-        60.00: "D-",
-    }
 
     for grade, score in score_to_grade_mapping.items():
         if Credit.raw_score_grade >= grade:
