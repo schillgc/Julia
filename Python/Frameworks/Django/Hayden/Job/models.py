b457a9b662d37fa3710e0eb917c90a4caaaa84f1
Gavin Clay Schilling <schillgc@hotmail.com>
2024-05-27 00:43:33 -0400

    Optimize code

--- Python/Frameworks/Django/Hayden/Job/models.py
+++ Python/Frameworks/Django/Hayden/Job/models.py
@@ -2,47 +2,37 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
+DEGREE_CHOICES = (
+  ("Minimum Age", "Minimum Age"),
+  ("High School Diploma", "High School Diploma"),
+  ("Associate's Degree", "Associate's Degree"),
+  ("Bachelor's Degree", "Bachelor's Degree"),
+  ("Master's of Business Administration", "Master's of Business Administration"),
+  ("Doctor of Jurisprudence", "Doctor of Jurisprudence"),
+  ("Master of Laws", "Master of Laws"),
+  ("Doctor of Medicine", "Doctor of Medicine"),
+  ("Doctor of Philosophy", "Doctor of Philosophy"),
+  ("Fellowship", "Fellowship")
+)
+
+
 class Career(models.Model):
-    profession = models.CharField(
-        verbose_name="Profession",
-        max_length=250,
+    profession = models.CharField(max_length=250)
+
+    average_salary = MoneyField(
+        max_digits=6,
+        decimal_places=0,
+        default_currency='USD'
     )
 
-    average_salary = MoneyField(max_digits=6, decimal_places=0, default_currency='USD')
-
-    MINIMUM_AGE = "Minimum Age"
-    HIGH_SCHOOL_DIPLOMA = "High School Diploma"
-    ASSOCIATES = "Associate's Degree"
-    BACHELORS = "Bachelor's Degree"
-    MBA = "Master's of Business Administration"
-    JD = "Doctor of Jurisprudence"
-    LLM = "Master of Laws"
-    MD = "Doctor of Medicine"
-    PHD = "Doctor of Philosophy"
-    FELLOW = "Fellowship"
-
-    DEGREE_CHOICES = [
-        (MINIMUM_AGE, "Minimum Age"),
-        (HIGH_SCHOOL_DIPLOMA, "High School Diploma"),
-        (ASSOCIATES, "Associate's Degree"),
-        (BACHELORS, "Bachelor's Degree"),
-        (MBA, "Master's of Business Administration"),
-        (JD, "Doctor of Jurisprudence"),
-        (LLM, "Master of Laws"),
-        (MD, "Doctor of Medicine"),
-        (PHD, "Doctor of Philosophy"),
-        (FELLOW, "Fellowship"),
-    ]
-
     required_education = models.CharField(
-        verbose_name="Required Education",
         max_length=35,
         choices=DEGREE_CHOICES,
-        blank=True,
+        blank=True
     )
 
     def __str__(self):
         return self.profession
 
     class Meta:
-        ordering = ['required_education', 'average_salary', 'profession']
+        ordering = ["required_education", "average_salary", "profession"]
